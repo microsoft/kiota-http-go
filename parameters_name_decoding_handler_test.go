a1a4f51e58661ae96e525a89ef73beebde6e2570
Vincent Biret <vibiret@microsoft.com>
2022-04-08 15:23:38 +0000

    - adds unit test for name decoder
    Signed-off-by: GitHub <noreply@github.com>

---
+++ parameters_name_decoding_handler_test.go
@@ -0,0 +1,43 @@
+package nethttplibrary
+
+import (
+	nethttp "net/http"
+	httptest "net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestItDecodesQueryParameterNames(t *testing.T) {
+	testData := [][]string{
+		{"?%24select=diplayName&api%2Dversion=2", "/?$select=diplayName&api-version=2"},
+		{"?%24select=diplayName&api%7Eversion=2", "/?$select=diplayName&api~version=2"},
+		{"?%24select=diplayName&api%2Eversion=2", "/?$select=diplayName&api.version=2"},
+		{"?%24select=diplayName&api%2Eversion=2", "/?$select=diplayName&api.version=2"},
+		{"?%24select=diplayName&api%2Dversion=2", "/?$select=diplayName&api-version=2"},
+		{"", "/"},
+	}
+	result := ""
+	testServer := httptest.NewServer(nethttp.HandlerFunc(func(res nethttp.ResponseWriter, req *nethttp.Request) {
+		result = req.URL.String()
+		res.WriteHeader(200)
+		res.Write([]byte("body"))
+	}))
+	defer func() { testServer.Close() }()
+	for _, data := range testData {
+
+		handler := NewParametersNameDecodingHandler()
+		input := testServer.URL + data[0]
+		expected := data[1]
+		req, err := nethttp.NewRequest(nethttp.MethodGet, input, nil)
+		if err != nil {
+			t.Error(err)
+		}
+		resp, err := handler.Intercept(newNoopPipeline(), 0, req)
+		if err != nil {
+			t.Error(err)
+		}
+		assert.NotNil(t, resp)
+		assert.Equal(t, expected, result)
+	}
+}
