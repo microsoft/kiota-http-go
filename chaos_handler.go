e7ff85494155e8d95b1de85d0816ea1db81c00cc
rkodev <43806892+rkodev@users.noreply.github.com>
2022-08-11 23:55:28 +0300

    Adds chaos handler

---
+++ chaos_handler.go
@@ -0,0 +1,228 @@
+package nethttplibrary
+
+import (
+	"errors"
+	"io"
+	"math/rand"
+	nethttp "net/http"
+	"regexp"
+	"strings"
+)
+
+type ChaosStrategy int
+
+const (
+	Manual ChaosStrategy = 0
+	Random               = 1
+)
+
+// ChaosHandlerOptions is a configuration struct holding options for a chaos handler
+//
+// BaseUrl represent the host url for in
+// ChaosStrategy Specifies the strategy used for the Testing Handler -> RANDOM/MANUAL
+// StatusCode Status code to be returned in the response
+// StatusMessage Message to be returned in the response
+// ChaosPercentage The percentage of randomness/chaos in the handler
+// ResponseBody The response body to be returned in the response
+// Headers The response headers to be returned in the response
+// StatusMap The Map passed by user containing url-statusCode info
+type ChaosHandlerOptions struct {
+	BaseUrl         string
+	ChaosStrategy   ChaosStrategy
+	StatusCode      int
+	StatusMessage   string
+	ChaosPercentage int
+	ResponseBody    *nethttp.Response
+	Headers         map[string][]string
+	StatusMap       map[string]map[string]int
+}
+
+type ChaosHandler struct {
+	options *ChaosHandlerOptions
+}
+
+// NewChaosHandlerWithOptions creates a new ChaosHandler with the configured options
+func NewChaosHandlerWithOptions(handlerOptions ChaosHandlerOptions) (*ChaosHandler, error) {
+	if handlerOptions.ChaosPercentage < 0 || handlerOptions.ChaosPercentage > 100 {
+		return nil, errors.New("ChaosPercentage must be between 0 and 100")
+	}
+	if handlerOptions.ChaosStrategy == Manual {
+		if handlerOptions.StatusCode == 0 {
+			return nil, errors.New("invalid status code for manual strategy")
+		}
+	}
+
+	return &ChaosHandler{options: &handlerOptions}, nil
+}
+
+// NewChaosHandler creates a new ChaosHandler with default configuration options of Random errors at 10%
+func NewChaosHandler() *ChaosHandler {
+	return &ChaosHandler{
+		options: &ChaosHandlerOptions{
+			ChaosPercentage: 10,
+			ChaosStrategy:   Random,
+			StatusMessage:   "A random error message",
+		},
+	}
+}
+
+var methodStatusCode = map[string][]int{
+	"GET":    {429, 500, 502, 503, 504},
+	"POST":   {429, 500, 502, 503, 504, 507},
+	"PUT":    {429, 500, 502, 503, 504, 507},
+	"PATCH":  {429, 500, 502, 503, 504429, 500, 502, 503, 504},
+	"DELETE": {429, 500, 502, 503, 504, 507},
+}
+
+var httpStatusCode = map[int]string{
+	100: "Continue",
+	101: "Switching Protocols",
+	102: "Processing",
+	103: "Early Hints",
+	200: "OK",
+	201: "Created",
+	202: "Accepted",
+	203: "Non-Authoritative Information",
+	204: "No Content",
+	205: "Reset Content",
+	206: "Partial Content",
+	207: "Multi-Status",
+	208: "Already Reported",
+	226: "IM Used",
+	300: "Multiple Choices",
+	301: "Moved Permanently",
+	302: "Found",
+	303: "See Other",
+	304: "Not Modified",
+	305: "Use Proxy",
+	307: "Temporary Redirect",
+	308: "Permanent Redirect",
+	400: "Bad Request",
+	401: "Unauthorized",
+	402: "Payment Required",
+	403: "Forbidden",
+	404: "Not Found",
+	405: "Method Not Allowed",
+	406: "Not Acceptable",
+	407: "Proxy Authentication Required",
+	408: "Request Timeout",
+	409: "Conflict",
+	410: "Gone",
+	411: "Length Required",
+	412: "Precondition Failed",
+	413: "Payload Too Large",
+	414: "URI Too Long",
+	415: "Unsupported Media Type",
+	416: "Range Not Satisfiable",
+	417: "Expectation Failed",
+	421: "Misdirected Request",
+	422: "Unprocessable Entity",
+	423: "Locked",
+	424: "Failed Dependency",
+	425: "Too Early",
+	426: "Upgrade Required",
+	428: "Precondition Required",
+	429: "Too Many Requests",
+	431: "Request Header Fields Too Large",
+	451: "Unavailable For Legal Reasons",
+	500: "Internal Server Error",
+	501: "Not Implemented",
+	502: "Bad Gateway",
+	503: "Service Unavailable",
+	504: "Gateway Timeout",
+	505: "HTTP Version Not Supported",
+	506: "Variant Also Negotiates",
+	507: "Insufficient Storage",
+	508: "Loop Detected",
+	510: "Not Extended",
+	511: "Network Authentication Required",
+}
+
+func generateRandomStatusCode(request *nethttp.Request) int {
+	statusCodeArray := methodStatusCode[request.Method]
+	return statusCodeArray[rand.Intn(len(statusCodeArray))]
+}
+
+func getRelativeURL(handlerOptions *ChaosHandlerOptions, url string) string {
+	baseUrl := handlerOptions.BaseUrl
+	if baseUrl != "" {
+		return strings.Replace(url, baseUrl, "", 1)
+	} else {
+		return url
+	}
+}
+
+func getStatusCode(handler *ChaosHandler, req *nethttp.Request) int {
+	handlerOptions := handler.options
+	requestMethod := req.Method
+	statusMap := handler.options.StatusMap
+	requestURL := req.RequestURI
+
+	if handlerOptions.ChaosStrategy == Manual {
+		return handler.options.StatusCode
+	}
+
+	if handlerOptions.ChaosStrategy == Random {
+		if handlerOptions.StatusCode > 0 {
+			return handlerOptions.StatusCode
+		} else {
+			relativeUrl := getRelativeURL(handlerOptions, requestURL)
+			definedResponses := statusMap[relativeUrl]
+			if definedResponses != nil {
+				mapCode := definedResponses[requestMethod]
+
+				if mapCode != 0 {
+					return mapCode
+				}
+			} else {
+				for key, _ := range statusMap {
+					match, _ := regexp.MatchString(key+"$", "peach")
+					if match {
+						responseCode := statusMap[key][requestMethod]
+						if responseCode != 0 {
+							return responseCode
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return generateRandomStatusCode(req)
+}
+
+func createResponseBody(handler *ChaosHandler, statusCode int) *nethttp.Response {
+	if handler.options.ResponseBody != nil {
+		return handler.options.ResponseBody
+	}
+
+	var stringReader *strings.Reader
+	if statusCode > 400 {
+		codeMessage := httpStatusCode[statusCode]
+		errMessage := handler.options.StatusMessage
+		stringReader = strings.NewReader("error : { code :  " + codeMessage + " , message : " + errMessage + " }")
+	} else {
+		stringReader = strings.NewReader("{}")
+	}
+
+	return &nethttp.Response{
+		StatusCode: statusCode,
+		Status:     handler.options.StatusMessage,
+		Body:       io.NopCloser(stringReader),
+		Header:     handler.options.Headers,
+	}
+}
+
+func createChaosResponse(handler *ChaosHandler, req *nethttp.Request) (*nethttp.Response, error) {
+	statusCode := getStatusCode(handler, req)
+	responseBody := createResponseBody(handler, statusCode)
+	return responseBody, nil
+}
+
+func (middleware ChaosHandler) Intercept(pipeline Pipeline, middlewareIndex int, req *nethttp.Request) (*nethttp.Response, error) {
+	if rand.Intn(100) < middleware.options.ChaosPercentage {
+		return createChaosResponse(&middleware, req)
+	}
+
+	return pipeline.Next(req, middlewareIndex)
+}
---
+++ chaos_handler_test.go
@@ -0,0 +1,55 @@
+package nethttplibrary
+
+import (
+	"github.com/stretchr/testify/assert"
+	nethttp "net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestItCreatesANewChaosHandler(t *testing.T) {
+	handler := NewChaosHandler()
+	if handler == nil {
+		t.Error("handler is nil")
+	}
+}
+
+func TestItCreatesANewChaosHandlerWithInvalidOptions(t *testing.T) {
+	_, err := NewChaosHandlerWithOptions(ChaosHandlerOptions{
+		ChaosPercentage: 101,
+		ChaosStrategy:   Random,
+	})
+	if err == nil || err.Error() != "ChaosPercentage must be between 0 and 100" {
+		t.Error("Expected initialization ")
+	}
+}
+
+func TestItCreatesANewChaosHandlerWithOptions(t *testing.T) {
+	options := ChaosHandlerOptions{
+		ChaosPercentage: 100,
+		ChaosStrategy:   Random,
+		StatusCode:      400,
+	}
+	handler, err := NewChaosHandlerWithOptions(options)
+	if err != nil {
+		t.Error(err)
+	}
+	if handler == nil {
+		t.Error("handler is nil")
+	}
+
+	testServer := httptest.NewServer(nethttp.HandlerFunc(func(res nethttp.ResponseWriter, req *nethttp.Request) {
+		res.WriteHeader(200)
+		res.Write([]byte("body"))
+	}))
+	req, err := nethttp.NewRequest(nethttp.MethodGet, testServer.URL, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	resp, err := handler.Intercept(newNoopPipeline(), 0, req)
+	if err != nil {
+		t.Error(err)
+	}
+	assert.NotNil(t, resp)
+	assert.Equal(t, 400, resp.StatusCode)
+}
